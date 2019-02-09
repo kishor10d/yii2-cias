f6526f02f40ddcf53fc65d99c8fc45eb84245f15
kishor10d <kishor10d@gmail.com>
2019-02-09 17:11:20 +0530

    adminLTE configuration started

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -9,15 +9,28 @@ use yii\web\AssetBundle;
  */
 class AppAsset extends AssetBundle
 {
-    public $basePath = '@webroot';
-    public $baseUrl = '@web';
-    public $css = [
-        'css/site.css',
-    ];
+    // public $basePath = '@webroot';
+    // public $baseUrl = '@web';
+    // public $css = [
+    //     'css/site.css',
+    // ];
+    // public $js = [
+    // ];
+    // public $depends = [
+    //     'yii\web\YiiAsset',
+    //     'yii\bootstrap\BootstrapAsset',
+    // ];
+
+    public $sourcePath = '@vendor/almasaeed2010/adminlte/plugins';
     public $js = [
+        'datatables/dataTables.bootstrap.min.js',
+        // more plugin Js here
+    ];
+    public $css = [
+        'datatables/dataTables.bootstrap.css',
+        // more plugin CSS here
     ];
     public $depends = [
-        'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
+        'dmstr\web\AdminLteAsset',
     ];
 }
